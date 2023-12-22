7e482f9698b77dcc94d8f74a72f6a7e4130264f4
Rafael Silva <nightmarezlolzin@gmail.com>
2023-12-22 20:36:03 -0300

    Resolução de problemas com testes unitários

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -10,12 +10,16 @@ ValidacoesLista lista = new ValidacoesLista();
 
 List<int> originalLista = new List<int> {1, -2, 3, -4, 5};
 List<int> listaSemNegativos = lista.RemoverNumerosNegativos(originalLista);
+List<int> listaMultiplicada = lista.MultiplicarNumerosLista(listaSemNegativos, 2);
 
 Console.WriteLine("Lista Original: " + string.Join(", ", originalLista));
 Console.WriteLine("Lista sem números negativos: " + string.Join(", ", listaSemNegativos));
+
+Console.WriteLine($"Lista Multiplicada por 2: {string.Join(", ", listaMultiplicada)}");
 
 int maiorNumero = lista.RetornarMaiorNumeroLista(originalLista);
 Console.WriteLine($"Maior Número da lista: {maiorNumero}");
 
 int menorNumero = lista.RetornarMenorNumeroLista(originalLista);
 Console.WriteLine($"Menor Número da lista: {menorNumero}");
+
