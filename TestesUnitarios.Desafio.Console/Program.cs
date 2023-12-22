720be78f82aadc105a7a5417b8a7144f1a4df0bd
Rafael Silva <nightmarezlolzin@gmail.com>
2023-12-22 10:43:20 -0300

    Instanciando a Classe

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TestesUnitarios.Desafio.Console.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+ValidacoesLista lista = new ValidacoesLista();
+
+// Utilizando métodos da Classe ValidacoesLista
+
+List<int> originalLista = new List<int> {1, -2, 3, -4, 5};
+List<int> listaSemNegativos = lista.RemoverNumerosNegativos(originalLista);
+
+Console.WriteLine("Lista Original: " + string.Join(", ", originalLista));
+Console.WriteLine("Lista sem números negativos: " + string.Join(", ", listaSemNegativos));
+
+int maiorNumero = lista.RetornarMaiorNumeroLista(originalLista);
+Console.WriteLine($"Maior Número da lista: {maiorNumero}");
+
+int menorNumero = lista.RetornarMenorNumeroLista(originalLista);
+Console.WriteLine($"Menor Número da lista: {menorNumero}");
